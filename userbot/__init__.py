494219fb544a877ec3f9f5a5e24a1e60a776596f
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2020-03-22 07:00:00 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -69,7 +69,7 @@ HEROKU_APIKEY = os.environ.get("HEROKU_APIKEY", None)
 
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/rekcah-pavi/javes")
+    "https://github.com/rekcahkumar/javes")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))

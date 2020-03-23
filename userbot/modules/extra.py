c81141df0282d1b55c650aee97622c29ee977863
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2020-03-23 08:57:57 +0530

    Update extra.py

--- userbot/modules/extra.py
+++ userbot/modules/extra.py
@@ -144,7 +144,7 @@ def bruh(name):
     os.system("instantmusic -q -s "+name)
     
 
-@register(outgoing=True, disable_errors=True, pattern="^.song(?: |$)(.*)")
+@javes05(outgoing=True, disable_errors=True, pattern="^.song(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return

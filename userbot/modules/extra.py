f4d146219870f346cb75f76cbad6602b065cbd84
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2020-03-24 07:26:11 +0530

    Add files via upload

--- userbot/modules/extra.py
+++ userbot/modules/extra.py
@@ -137,38 +137,6 @@ DOGBIN_URL = "https://del.dog/"
 
 
 
-os.system("rm -rf *.mp3")
-
-
-def bruh(name):
-    
-    os.system("instantmusic -q -s "+name)
-    
-
-@javes05(outgoing=True, pattern="^.song(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    DELAY_BETWEEN_EDITS = 0.3
-    PROCESS_RUN_TIME = 100
-    cmd = event.pattern_match.group(1)
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    await event.edit("ok finding the song")    
-    bruh(str(cmd))
-    l = glob.glob("*.mp3")
-    await event.edit("sending song")
-    await bot.send_file(
-                event.chat_id,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id
-            )
-    os.system("rm -rf *.mp3")
-    subprocess.check_output("rm -rf *.mp3",shell=True)
-
 
 
 

e51ea0db35df17ff1e021c1cbee50b7b3c93d83f
rekcahkumar <48340011+rekcahkumar@users.noreply.github.com>
2020-03-24 11:32:42 +0530

    Add files via upload

--- userbot/modules/system.py
+++ userbot/modules/system.py
@@ -64,7 +64,7 @@ async def subprocess_run(cmd, heroku):
     return stdout.decode().strip(), stderr.decode().strip(), exitCode
 
 
-@register(outgoing=True, pattern=r"^!heroku(?: |$)(.*)")
+@javes05(outgoing=True, pattern=r"^!heroku(?: |$)(.*)")
 async def heroku_manager(heroku):
     await heroku.edit("`Processing...`")
     await asyncio.sleep(3)
